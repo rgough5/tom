cd7a74bd37ac5d8ff0ab0a0072e89fbe8ea97ecd
rgough5 <78178913+rgough5@users.noreply.github.com>
2024-05-21 18:46:09 -0400

    Update preview.py

--- preview.py
+++ preview.py
@@ -8,7 +8,7 @@ import time
 from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
+picam2.start_preview(Preview.QT)
 
 preview_config = picam2.create_preview_configuration({'size': (1280, 720)}, controls={'FrameDurationLimits': (100000, 100000), 'ExposureTime': 700})
 picam2.configure(preview_config)
