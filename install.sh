4615e4ff6c1a84d90341fe6cfce17f1dbc7973c4
rgough5 <rgough5@jhu.edu>
2024-05-31 15:40:24 -0400

    updated installer to be used on both

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y libportaudio2 ffmpeg python3-pip python3-cffi python3-numpy
+sudo apt install -y libportaudio2 ffmpeg python3-pip python3-cffi python3-numpy python3-pyqt5 python3-opengl python3-picamera2
 sudo python -m venv --system-site-packages /opt/rec/
 source /opt/rec/bin/activate
 pip install sounddevice soundfile
 curl -O https://raw.githubusercontent.com/rgough5/tom/main/a_tom.py
+curl -O https://raw.githubusercontent.com/rgough5/tom/main/v_tom.py
 chmod +x a_tom.py
--- v_tom.py
+++ v_tom.py
@@ -5,9 +5,10 @@
 # The preview window will close and the recording starts after inidcating
 # whether the user wants to record audio on a connected pi.
 
-from picamera2 import Picamera2, Preview
-from picamera2.encoders import H264Encoder, Quality
+from picamera2 import Picamera2, Preview, controls
+from picamera2.encoders import H264Encoder, JpegEncoder, Quality
 from picamera2.outputs import FfmpegOutput
+from libcamera import controls
 from datetime import datetime # what a dumb import
 import time
 from gpiozero import DigitalOutputDevice, DigitalInputDevice
@@ -18,13 +19,13 @@ import subprocess
 # nor longer than the second. FR = 1000000 / frame_duration. Common settings:
 # 10 FPS = (100000, 100000), 30 FPS = (33333, 33333), 25 FPS (40000, 40000)
 
-def recV(fname, dur, enc, bitrate, a='n'):
+def recV(picam, fname, dur, enc, bitrate, a='n'):
     start_pin = DigitalOutputDevice(pin=26)
     audio_ready = DigitalInputDevice(pin=19)
     if a == 'y':
         audio_ready.wait_for_active()
     out = FfmpegOutput(fname+'.mp4')
-    picam.start_encoder(enc, out, quality=Quality.MEDIUM)
+    picam.start_encoder(enc, out, quality=Quality.LOW)
     picam.start()
     start_pin.on()
     print('recording ' + fname)
@@ -37,28 +38,31 @@ def recV(fname, dur, enc, bitrate, a='n'):
 if __name__=='__main__':
     sz= (1920, 1080)
     frame_duration=(100000, 100000)
-    exposure=500
+    exposure=1000
+    sharpness=4
     seg=3600
+
     ### setup section ###
-    fname = input("File prefix: ")
-    t = int(input("Time in seconds to record: "))
     # fname = sys.argv[1]
     # t = int(sys.argv[2])
 
     picam = Picamera2()
-    cam_config = picam.create_video_configuration({'size': (1920,1080)}, controls={'FrameDurationLimits': frame_duration, 'ExposureTime': exposure})
+    cam_config = picam.create_video_configuration({'size': sz}, controls={'FrameDurationLimits': frame_duration, 'ExposureTime': exposure, 'Saturation': 0, 'NoiseReductionMode': controls.draft.NoiseReductionModeEnum.Off, 'Sharpness': sharpness})
     picam.configure(cam_config)
     auto = 'a'
     encoder = H264Encoder()
     print('Adjust camera as desired. Recording will start after audio selection')
     print('Alternatively, cancel now with Ctrl+C')
-    time.sleep(1)
-    picam.start_preview(Preview.QT)
+    picam.start_preview(Preview.QT) #bad way to preview, but necessary to view over ssh
     picam.start()
+    fname = input("File prefix: ")
+    t = int(input("Time in seconds to record: "))
     while not(auto.lower() == 'y' or auto.lower() == 'n'):
         auto = input('Record with audio (y/n)? ')
-
-    picam.stop_preview()
+    try:
+        picam.stop_preview()
+    except:
+        pass
     picam.stop()
 
     ### recording section ###
@@ -69,11 +73,11 @@ if __name__=='__main__':
     i = 0
     while i < t//seg:
         i_fname = '{}_{}_{:%m%d%y-%H%M%S}'.format(fname, str(i), datetime.now())
-        recV(i_fname, seg, encoder, auto.lower())
+        recV(picam, i_fname, seg, encoder, auto.lower())
         i += 1
 
     ft = t%seg
     if ft != 0:
         i_fname = '{}_{}_{:%m%d%y-%H%M%S}'.format(fname, str(i), datetime.now())
-        recV(i_fname, ft, encoder, auto.lower())
+        recV(picam, i_fname, ft, encoder, auto.lower())
 
