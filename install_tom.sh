7c4d6e031a47ac35b5a10fac9e0114ad9fadc572
rgough5 <78178913+rgough5@users.noreply.github.com>
2024-06-13 15:52:41 -0400

    Update and rename install.sh to install_tom.sh

--- install_tom.sh
+++ install_tom.sh
@@ -5,5 +5,5 @@ sudo apt install -y libportaudio2 ffmpeg python3-pip python3-cffi python3-numpy 
 sudo python -m venv --system-site-packages /opt/rec/
 source /opt/rec/bin/activate
 pip install sounddevice soundfile
-curl -O https://raw.githubusercontent.com/rgough5/tom/solo/tom.py
+curl -O https://raw.githubusercontent.com/rgough5/tom/dev/tom.py
 chmod +x tom.py
